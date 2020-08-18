65d3c4fbb993d6070a2275b672a026b8317cb4cd
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-08-18 10:56:16 +0700

    Hangling Events

--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -6,7 +6,7 @@ class TodoItem extends Component{
     render(){
         const {item, onClick} = this.props;
         return(
-            <div onClick ={onClick} className ={classNames('TodoItem',{
+            <div onClick ={onClick } className ={classNames('TodoItem',{
                 'TodoItem-complete' :item.isComplete
             })}>
                 <p>{this.props.item.title}</p>
